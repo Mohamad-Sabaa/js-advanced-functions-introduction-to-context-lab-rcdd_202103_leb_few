04b3d95617ed1488d20094d69c4156fde204ac2e
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-05-16 12:11:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function createTimeOutEvent(obj, fullDate) {
 }
 
 function hoursWorkedOnDate(obj, date) {
-  let timeIn = obj.timeInEvents.find(e => e.date === date)
-  let timeOut = obj.timeOutEvents.find(e => e.date === date)
-  return let hoursWokerd = timeIn.hour - timeOut.hour
+  let timeIn = obj.timeInEvents.find(e => e.date === date);
+  let timeOut = obj.timeOutEvents.find(e => e.date === date);
+  return let hoursWokerd = timeIn.hour - timeOut.hour;
 }
