e16a035bb0f595a9523ce6c509d574fedd9034e2
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-05-16 11:06:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+function createEmployeeRecord (testEmployee) {
+   testEmployee = {
+     firstName: "Gray",
+     familyName: "Worm",
+     title: "Security",
+     payPerHour: 1,
+     timeInEvents: [],
+     timeOutEvents: []
+   }
+}
